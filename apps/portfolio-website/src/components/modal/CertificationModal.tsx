0fc2137561c592f0c9fd7cca32d8b96e97eb9548
Tyche-sama <158128311+tychesama@users.noreply.github.com>
2026-01-18 21:58:00 +0800

    feat: certification modal filler information

--- apps/portfolio-website/src/components/modal/CertificationModal.tsx
+++ apps/portfolio-website/src/components/modal/CertificationModal.tsx
@@ -17,34 +17,23 @@ interface CertificationsCardProps {
 
 const CertificationModal = () => {
   return (
-    <div className="flex flex-col gap-4 w-[900px]">
-      <div className="flex flex-row items-center justify-center h-[284px]">
-        <div className="bg-[--color-card] py-2 px-3 h-[284px] flex flex-col items-center gap-3 shadow-sm rounded-lg">
+    <div className="flex flex-row gap-4 w-[668px] p-2 h-[314px]">
+      <div className="flex flex-row items-center justify-center">
+        <div className="bg-[--color-card] py-2 px-3 h-[284px] w-[240px] flex flex-col items-center gap-3 shadow-sm rounded-lg">
           <img
             src="/assets/ccna.jpg"
             alt="logo"
             className="w-[200px] h-[200px] rounded-lg object-cover"
           />
-          <p>Jairosoft Inc</p>
-          <p>Bubble Developer</p>
-
-        </div>
-        <div className="py-2 px-3 h-[284px] flex items-center gap-3 shadow-sm hover:shadow-md transition-shadow duration-150">
-          <img
-            src="/assets/sss.jpg"
-            alt="Loading..."
-            className="w-[200px] h-[200px] rounded-lg object-cover"
-          />
+          <p>CCNA</p>
+          <p>Networking</p>
         </div>
       </div>
-      <div className="flex flex-col items-start justify-start h-[215px]">
+      <div className="flex flex-col items-start justify-start">
         <div>
-          <p>About the Company</p>
-          <p> descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long description</p>
-        </div>
-        <div>
-          <p>About the Company</p>
-          <p> Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long descriptionVery Long description</p>
+          <p>Certificiate Title</p>
+          <p> Cert Description</p>
+          <p>Date</p>
         </div>
       </div>
     </div>
