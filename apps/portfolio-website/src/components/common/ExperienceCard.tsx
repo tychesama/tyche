cbcaf84389e5a007e1e6ba08befe7abb1093d19d
Tyche-sama <158128311+tychesama@users.noreply.github.com>
2026-02-03 21:15:37 +0800

    feat-profile: hover on experience card

--- apps/portfolio-website/src/components/common/ExperienceCard.tsx
+++ apps/portfolio-website/src/components/common/ExperienceCard.tsx
@@ -18,7 +18,7 @@ interface ExperienceCardProps {
 
 const ExperienceCard: React.FC<ExperienceCardProps> = ({ experience }) => {
   return (
-    <div className="bg-[var(--color-mini-card)] rounded-lg shadow-md p-5 flex flex-col h-[120px] w-[246px]">
+    <div className="bg-[var(--color-mini-card)] rounded-lg shadow-md p-5 flex flex-col h-[120px] w-[246px] transition transform hover:scale-[1.03]">
       <div className="flex flex items-center justify-center flex-grow gap-2">
         <img
           src={experience.logo}
