ab477cbfe500bfb0342d4d2d5022125c81e11dcf
Tyche-sama <158128311+tychesama@users.noreply.github.com>
2026-02-05 17:39:30 +0800

    feat: overhaul hero section

--- apps/portfolio-website/src/components/sections/HeroSection.tsx
+++ apps/portfolio-website/src/components/sections/HeroSection.tsx
@@ -1,15 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaFacebook, FaLinkedin, FaXTwitter, FaInstagram, FaGithub, FaYoutube, FaFigma } from "react-icons/fa6";
 
-interface ProfileProps {
-  profile: {
-    name: string;
-    title: string;
-    nickname: string;
-    image: string;
-    about: string;
-  };
-}
 
 interface Article {
   id: string;
@@ -36,95 +30,127 @@ const ProfileDefault: React.FC<{ profile: any }> = ({ profile }) => {
   }, []);
 
   return (
-    <div className="w-full h-full flex flex-col justify-between items-center text-[var(--color-text-main)]">
-      {/* profile section */}
-      <div className="px-8 pt-6 rounded-lg flex items-start w-full ml-8 ">
-        <div className="min-w-[275px] h-[320px] overflow-hidden">
-          {/* w-[300px] h-[640px] w-[340px] h-[1150px]*/}
-          <img 
-            src="/assets/sss.jpg"
-            alt="Profile"
-            className="w-[300px] h-[640px] object-cover object-top transform"
-          />
-        </div>
-        <div>
-          <p className="mt-20 mb-4 text-[30px] font-bold tracking-wide text-[var(--color-text-main)]"> 
-            Hi, my name is Joem!
-          </p>
-          <p className="text-base text-[var(--color-text-subtle)] leading-relaxed italic">
-            { /*profile.about */ } This website is in work in progress!
-          </p>
+    <div className="w-full h-full flex items-center justify-center mt-[4px]">
+      <div className="w-[875px] h-[550px] w-full flex items-start bg-[rgba(0,0,0,0.18)] backdrop-blur-[2px] rounded-lg shadow-[inset_0_6px_16px_rgba(0,0,0,0.45)]  rounded-xl shadow-inner overflow-hidden">
+        <div className="w-full h-full flex flex-col justify-between items-center text-[var(--color-text-main)]">
+          {/* profile section */}
+          <div className="w-full px-10 pt-10">
+            <div className="w-full flex items-start gap-10">
+
+              {/* profile image */}
+              <div className="w-[320px] h-[320px] flex-shrink-0 rounded-full overflow-hidden bg-[rgba(0,0,0,0.18)] border border-[rgba(255,255,255,0.06)] shadow-[inset_0_10px_26px_rgba(0,0,0,0.55),_0_18px_40px_rgba(0,0,0,0.35)]">
+                <img
+                  src="/assets/pfp_new.png"
+                  alt="Profile"
+                  className="w-full h-full object-cover object-top"
+                />
+              </div>
+
+              {/* text content */}
+              <div className="flex-1 flex flex-col justify-center items-start gap-4 text-left">
+                <p className="text-[34px] font-bold tracking-wide leading-tight">
+                  Hi, my name is Joem!
+                </p>
+                <div className=" text-justify text-sm text-[var(--color-text-subtle)] leading-relaxed max-w-[560px] space-y-4">
+                  <p>
+                    This website is still a work in progress. I am gradually building features,
+                    refining the design, and experimenting with different ideas as I continue
+                    learning and improving my development skills.
+                  </p>
+                  <p>
+                    You may notice changes over time as new sections are added, layouts are
+                    adjusted, and projects are updated. The goal is to create a space that
+                    reflects both my work and my growth as a developer.
+                  </p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+          {/* latest article card */}
+          {latestArticle && (
+            <div className="w-[875px] h-[175px] bg-gradient-to-b from-[var(--color-mini-card)] to-[color-mix(in_srgb,var(--color-mini-card)_65%,black)] p-6 rounded-md flex items-center justify-center gap-4 shadow-md">
+              <div className="flex flex-col gap-[10px] w-[600px] h-full divide-y divide-[var(--color-text-subtle)]">
+                <div className="w-[600px] h-[90px] flex flex-row gap-[9px] items-start">
+                  <Image
+                    src="/assets/asd.jpg"
+                    alt={latestArticle.title}
+                    width={75}
+                    height={75}
+                    className="w-[70px] h-[70px] rounded-md object-cover"
+                  />
+                  <div className="w-full h-full flex flex-col justify-center items-start">
+                    <p className="text-lg font-semibold text-[var(--color-text-main)] hover:text-[var(--color-text-subtle)] hover:underline truncate">{latestArticle.title}</p>
+                    <p className="text-sm text-[var(--color-text-subtle)]">{latestArticle.description}</p>
+                  </div>
+                </div>
+
+                <div className="w-[600px] h-full text-sm text-[var(--color-text-subtle)] px-2 flex items-center gap-2">
+                  <span>Latest article of my Blog!</span>
+                  <span className="opacity-60">|</span>
+                  <Link href="https://example.com/blog" className="text-sm text-[var(--color-text-subtle)] hover:text-[var(--color-text-subtle)] hover:underline truncate">
+                    Check out my Blog here!
+                  </Link>
+                  <span className="opacity-60">|</span>
+                  <span className="text-sm text-[var(--color-text-subtle)]">
+                    Check out my Social Media {"->"}
+                  </span>
+                </div>
+              </div>
+
+              <div className="w-[210px] h-full flex flex-col justify-center -mt-4">
+                <p className="text-sm text-[var(--color-text-main)] mb-2">Links:</p>
+
+                <div className="grid grid-cols-4 auto-rows-[40px] w-[190px] gap-[10px] ml-[15px]">
+                  <a href="https://example.com/facebook" target="_blank" rel="noopener noreferrer" aria-label="facebook"
+                    className="group w-[40px] h-[40px] rounded-md bg-[rgba(255,255,255,0.04)] hover:bg-[rgba(255, 255, 255, 0)] border border-[rgba(255,255,255,0.01)] flex items-center justify-center hover:shadow-md transition-all duration-150">
+                    <FaFacebook className="text-[24px] text-[var(--color-text-main)] group-hover:text-[var(--color-text-subtle)]" />
+                  </a>
+
+                  <a href="https://example.com/linkedin" target="_blank" rel="noopener noreferrer" aria-label="linkedin"
+                    className="group w-[40px] h-[40px] rounded-md bg-[rgba(255,255,255,0.04)] hover:bg-[rgba(255, 255, 255, 0)] border border-[rgba(255,255,255,0.01)] flex items-center justify-center hover:shadow-md transition-all duration-150">
+                    <FaLinkedin className="text-[24px] text-[var(--color-text-main)] group-hover:text-[var(--color-text-subtle)]" />
+                  </a>
+
+                  <a href="https://example.com/twitter" target="_blank" rel="noopener noreferrer" aria-label="twitter"
+                    className="group w-[40px] h-[40px] rounded-md bg-[rgba(255,255,255,0.04)] hover:bg-[rgba(255, 255, 255, 0)] border border-[rgba(255,255,255,0.01)] flex items-center justify-center hover:shadow-md transition-all duration-150">
+                    <FaXTwitter className="text-[24px] text-[var(--color-text-main)] group-hover:text-[var(--color-text-subtle)]" />
+                  </a>
+
+                  <a href="https://example.com/instagram" target="_blank" rel="noopener noreferrer" aria-label="instagram"
+                    className="group w-[40px] h-[40px] rounded-md bg-[rgba(255,255,255,0.04)] hover:bg-[rgba(255, 255, 255, 0)] border border-[rgba(255,255,255,0.01)] flex items-center justify-center hover:shadow-md transition-all duration-150">
+                    <FaInstagram className="text-[24px] text-[var(--color-text-main)] group-hover:text-[var(--color-text-subtle)]" />
+                  </a>
+
+                  <a href="https://example.com/github-main" target="_blank" rel="noopener noreferrer" aria-label="github_main"
+                    className="group w-[40px] h-[40px] rounded-md bg-[rgba(255,255,255,0.04)] hover:bg-[rgba(255, 255, 255, 0)] border border-[rgba(255,255,255,0.01)] flex items-center justify-center hover:shadow-md transition-all duration-150">
+                    <FaGithub className="text-[24px] text-[var(--color-text-main)] group-hover:text-[var(--color-text-subtle)]" />
+                  </a>
+
+                  <a href="https://example.com/github-alt" target="_blank" rel="noopener noreferrer" aria-label="github_alt"
+                    className="group w-[40px] h-[40px] rounded-md bg-[rgba(255,255,255,0.04)] hover:bg-[rgba(255, 255, 255, 0)] border border-[rgba(255,255,255,0.01)] flex items-center justify-center hover:shadow-md transition-all duration-150">
+                    <FaGithub className="text-[24px] text-[var(--color-text-main)] group-hover:text-[var(--color-text-subtle)]" />
+                  </a>
+
+                  <a href="https://example.com/youtube" target="_blank" rel="noopener noreferrer" aria-label="youtube"
+                    className="group w-[40px] h-[40px] rounded-md bg-[rgba(255,255,255,0.04)] hover:bg-[rgba(255, 255, 255, 0)] border border-[rgba(255,255,255,0.01)] flex items-center justify-center hover:shadow-md transition-all duration-150">
+                    <FaYoutube className="text-[24px] text-[var(--color-text-main)] group-hover:text-[var(--color-text-subtle)]" />
+                  </a>
+
+                  <a href="https://example.com/figma" target="_blank" rel="noopener noreferrer" aria-label="figma"
+                    className="group w-[40px] h-[40px] rounded-md bg-[rgba(255,255,255,0.04)] hover:bg-[rgba(255, 255, 255, 0)] border border-[rgba(255,255,255,0.01)] flex items-center justify-center hover:shadow-md transition-all duration-150">
+                    <FaFigma className="text-[24px] text-[var(--color-text-main)] group-hover:text-[var(--color-text-subtle)]" />
+                  </a>
+                </div>
+              </div>
+
+            </div>
+          )}
+
         </div>
       </div>
-
-      {/* latest article card */}
-      {latestArticle && (
-        <div className="w-[850px] h-[175px] bg-gradient-to-b from-[var(--color-mini-card)] to-[color-mix(in_srgb,var(--color-mini-card)_65%,black)] p-6 rounded-md flex items-center justify-center gap-4 shadow-md">
-          <div className="flex flex-col gap-[10px] w-[600px] h-full divide-y divide-[var(--color-text-subtle)]">
-            <div className="w-[600px] h-[90px] flex-row gap-[9px] flex items-start">
-              <img
-                src='assets/sss.jpg'
-                alt={latestArticle.title}
-                className="w-[70px] h-[70px] rounded-md object-cover"
-              />
-              <div className="w-full h-full flex flex-col justify-center items-start">
-                <p className="text-lg text-[var(--color-text-main)]">
-                  {latestArticle.title}
-                </p>
-                <p className="text-sm text-[var(--color-text-subtle)]">
-                  {latestArticle.description}
-                </p>
-              </div>
-            </div>
-            <div className="w-[600px] h-full text-sm text-[var(--color-text-subtle)] px-4 flex items-center">
-              Random Text Random TextRandom TextRandom TextRandom 
-            </div>
-          </div>
-          <div className="grid grid-cols-4 auto-rows-[35px] w-[190px] h-[100px] gap-[10px] items-center justify-center p-[10px]">
-            <img
-              src='assets/sss.jpg'
-              alt={latestArticle.title}
-              className="w-[35px] h-[35px] rounded-md object-cover"
-            />
-            <img
-              src='assets/as.png'
-              alt={latestArticle.title}
-              className="w-[35px] h-[35px] rounded-md object-cover"
-            />
-            <img
-              src='assets/jairo.png'
-              alt={latestArticle.title}
-              className="w-[35px] h-[35px] rounded-md object-cover"
-            />
-            <img
-              src='assets/sss.jpg'
-              alt={latestArticle.title}
-              className="w-[35px] h-[35px] rounded-md object-cover"
-            />
-            <img
-              src='assets/sss.jpg'
-              alt={latestArticle.title}
-              className="w-[35px] h-[35px] rounded-md object-cover"
-            />
-            <img
-              src='assets/addu.png'
-              alt={latestArticle.title}
-              className="w-[35px] h-[35px] rounded-md object-cover"
-            />
-            <img
-              src='assets/sss.jpg'
-              alt={latestArticle.title}
-              className="w-[35px] h-[35px] rounded-md object-cover"
-            />
-            <img
-              src='assets/sss.jpg'
-              alt={latestArticle.title}
-              className="w-[35px] h-[35px] rounded-md object-cover"
-            />
-          </div>
-
-        </div>
-      )}
     </div>
   );
 };
